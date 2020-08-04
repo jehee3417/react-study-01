8e0edd88afaf423ce9b16aa544143aaa711fa354
jeheeLee <jhlee1@polarium.co.kr>
2020-08-04 16:13:22 +0900

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.set('views', __dirname );
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+app.get('/',(req,res)=>{
+  res.send('Hello World!');
+})
+
+var lectureRouter = require('./routes/lecture');
+app.use(lectureRouter);
+
+var gugudanRouter = require('./routes/gugudan');
+app.use(gugudanRouter);
+
+app.listen(3000,(req,res)=>{
+  console.log('Connected, 3000 port!')
+})
